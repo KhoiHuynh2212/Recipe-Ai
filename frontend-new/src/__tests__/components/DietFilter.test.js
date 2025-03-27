6b804d7936c3e129c4563efe54811b6bb8645623
StephenRanieri <stephen.r.ranieri@gmail.com>
2025-03-27 18:27:41 -0500

    Fixed DietFilter test

--- frontend-new/src/__tests__/components/DietFilter.test.js
+++ frontend-new/src/__tests__/components/DietFilter.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import DietFilter from '../../components/ChatUI/DietFilter';
 
-test('renders DietFilter component without crashing', () => {
+test('renders meal type component without crashing', () => {
     render(<DietFilter />);
     expect(screen.getByLabelText(/choose diet preference/i)).toBeInTheDocument();
 });
